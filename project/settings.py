44005a92691f28be43fcd50c5ba3275e97d1c159
Akshita Chadha <akshitachadha01@gmail.com>
2025-06-19 14:18:48 +0530

    commit

--- project/settings.py
+++ project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ssxsw*r8_62^zj!zl-lwienn1t@i&zc3+&!9*wkafc4nd^ig0'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['news-nexus-ia8c.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
